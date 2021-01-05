c86cf0e72ceeac705a1d995b9aff02a184df578d
UOrb <798008773@qq.com>
2021-01-06 07:53:59 +0800

    feat: init exec 询问是否清空目录

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const fs = require('fs')
+const fsExtra = require('fs-extra')
+const inquirer = require('inquirer')
+
 const log = require("@weilai-cli/log")
 const Command = require('@weilai-cli/command')
 
@@ -11,9 +15,52 @@ class initCommand extends Command {
         log.verbose('force',this.force)
     }
 
-    exec() {
-        // init 的业务逻辑
-        console.log('init 的业务逻辑')
+    async exec() {
+        try {
+            // init 的业务逻辑
+            log.verbose('init 的业务逻辑')
+
+            // 1. 准备阶段
+            await this.prepare()
+            // 2. 下载模板
+            // 3. 安装模板
+        } catch (e) {
+            log.error(e.message)
+        }
+    }
+
+    // 准备阶段
+    async prepare() {
+        const localPath = process.cwd()
+        // 1. 判断当前目录是否为空
+        if(!this.isCwdEmpty(localPath)) {
+            // 2. 询问是否启动强制更新
+            const { ifContinue } = await inquirer.prompt({
+                type: 'confirm',
+                name: 'ifContinue',
+                default: false,
+                message: '当前文件夹不为空，是否继续创建项目？'
+            })
+            console.log(ifContinue)
+            if( ifContinue ) {
+                // 清空当前目录
+                fsExtra.emptyDirSync(localPath)
+            }
+        }
+        
+        // 3. 选择创建项目或者组件
+        // 4. 获取项目的基本信息
+
+    }
+
+    // 判断当前目录是否为空
+    isCwdEmpty(localPath) {
+        let fileList = fs.readdirSync(localPath)
+        // 文件过滤的逻辑
+        fileList = fileList.filter(file => (
+            !file.startsWith('.') && !['node_modules'].includes(file)
+        ))
+        return !fileList || fileList.length <= 0
     }
 }
 
